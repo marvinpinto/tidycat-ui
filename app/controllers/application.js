1f52bb92e800a43824a5201e2745c108699be071
Marvin Pinto <marvin@pinto.im>
2016-03-23 08:40:16 -0400

    Use JWT in addition to the GitHub OAuth2 application-based authentication
    This allows the tidycat backend to validate requests using a mechanism
    (JWT) that is independent of the primary authentication mechanism
    (GitHub).
    
    Read more about it at: https://disjoint.ca/til/2016/03/22/chaining-authentication-in-an-ember-app
    
    Right now, the mock server backend responds on two endpoints:
    
    - `/api/auth/token`
    - `/api/auth/refresh`
    
    The `token` endpoint hands out a new JWT token, whereas the `refresh`
    endpoint validates the previous JWT and then hands out a new one.
    
    The idea here is to keep this mock backend in place until I get the real
    one up and running!

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,11 +3,21 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   session: Ember.inject.service('session'),
   actions: {
+
     logout() {
       this.get('session').invalidate();
     },
+
     login() {
-      this.get('session').authenticate('authenticator:torii', 'github');
+      var _this = this;
+      this.get('session').authenticate('authenticator:torii', 'github').then(function() {
+        var authorizationCode = _this.get('session.data.authenticated.authorizationCode');
+        var payload = {
+          password: authorizationCode
+        };
+        _this.get('session').authenticate('authenticator:jwt', payload);
+      });
     }
+
   }
 });
