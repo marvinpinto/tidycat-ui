91e153d78c6900d87925418e35bb6eb142de394b
Marvin Pinto <marvin@pinto.im>
2016-03-29 09:24:39 -0400

    Use a cleaner method for chaining ESA authentication
    Since ESA triggers the sessionAuthenticated method post-authentication,
    that place seemed like a reasonable choice to trigger the JWT
    authentication.
    
    Without this, there was a race condition between authentication
    finishing and the session object being ready to use, which resulted in
    the model (in the "authenticatedRoute") being in a bad state.

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,5 +1,20 @@
 import Ember from 'ember';
+import config from '../config/environment';
 import ApplicationRouteMixin from
   'ember-simple-auth/mixins/application-route-mixin';
 
-export default Ember.Route.extend(ApplicationRouteMixin, {});
+export default Ember.Route.extend(ApplicationRouteMixin, {
+
+  sessionAuthenticated: function() {
+    var authorizationCode = this.get('session.data.authenticated.authorizationCode');
+    var _this = this;
+    var authenticatedRoute = config['ember-simple-auth'].routeAfterAuthentication;
+    var payload = {
+      password: authorizationCode
+    };
+    this.get('session').authenticate('authenticator:jwt', payload).then(function() {
+      _this.transitionTo(authenticatedRoute);
+    });
+  }
+
+});
