marvinpinto/tidycat-ui

commit 813408084052ff95ea3ccb722ea7fcd0aec65abc

Use environment variables to distinguish configuration
The tldr here is that ember basically only knows how to deal with three
environments: testing, development, and production. So my attempts at
creating a "staging" environment does not work out so well.

(This strategy is basically what the Ember folks recommend)

config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   // **********
   var ENV = {
     modulePrefix: 'tidycat-ui',
-    environment: environment,
+    environmen: environment,
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
@@ -23,7 +23,9 @@ module.exports = function(environment) {
       sessionServiceName: 'session',
       providers: {
         'github-oauth2': {
-          scope: 'user'
+          scope: 'user',
+          apiKey: process.env.EMBER_GITHUB_APIKEY,
+          redirectUri: process.env.EMBER_GITHUB_REDIRECT_URI
         }
       }
     },
@@ -38,7 +40,9 @@ module.exports = function(environment) {
       authorizationHeaderName: 'Authorization',
       refreshAccessTokens: true,
       refreshLeeway: 300,
-      timeFactor: 1000
+      timeFactor: 1000,
+      serverTokenEndpoint: process.env.EMBER_ESA_TOKEN_ENDPOINT,
+      serverTokenRefreshEndpoint: process.env.EMBER_ESA_REFRESH_ENDPOINT
     }
 
   };
@@ -52,36 +56,6 @@ module.exports = function(environment) {
     ENV.APP.LOG_ACTIVE_GENERATION = false;
     ENV.APP.LOG_VIEW_LOOKUPS = false;
     ENV.APP.rootElement = '#ember-testing';
-  }
-
-  // *************
-  //  Development
-  // *************
-  if (environment === 'development') {
-    ENV.torii.providers['github-oauth2'].apiKey = 'a23e0b40bb0377aa6860';
-    ENV.torii.providers['github-oauth2'].redirectUri = 'http://127.0.0.1:4200';
-    ENV['ember-simple-auth-token'].serverTokenEndpoint = "http://127.0.0.1:8080/auth/token";
-    ENV['ember-simple-auth-token'].serverTokenRefreshEndpoint = "http://127.0.0.1:8080/auth/refresh";
-  }
-
-  // *********
-  //  Staging
-  // *********
-  if (environment === 'staging') {
-    ENV.torii.providers['github-oauth2'].apiKey = 'caf40f799d653c2ca635';
-    ENV.torii.providers['github-oauth2'].redirectUri = 'https://my-staging.tidycat.io';
-    ENV['ember-simple-auth-token'].serverTokenEndpoint = "https://api-staging.tidycat.io/auth/token";
-    ENV['ember-simple-auth-token'].serverTokenRefreshEndpoint = "https://api-staging.tidycat.io/auth/refresh";
-  }
-
-  // ************
-  //  Production
-  // ************
-  if (environment === 'production') {
-    ENV.torii.providers['github-oauth2'].apiKey = '89a218832dc2e39f575b';
-    ENV.torii.providers['github-oauth2'].redirectUri = 'https://my.tidycat.io';
-    ENV['ember-simple-auth-token'].serverTokenEndpoint = "https://api.tidycat.io/auth/token";
-    ENV['ember-simple-auth-token'].serverTokenRefreshEndpoint = "https://api.tidycat.io/auth/refresh";
   }
 
   return ENV;

Makefile
@@ -29,9 +29,34 @@ install:  ## Install project dependencies
 .PHONY: server
 server:  ## Run the ember server locally
 	(command -v watchman >/dev/null 2>&1 && watchman watch-del `pwd` && watchman watch-project `pwd`) || true
-	ember server \
+	EMBER_GITHUB_APIKEY="a23e0b40bb0377aa6860" \
+		EMBER_GITHUB_REDIRECT_URI="http://127.0.0.1:4200" \
+		EMBER_ESA_TOKEN_ENDPOINT="http://127.0.0.1:8080/auth/token" \
+		EMBER_ESA_REFRESH_ENDPOINT="http://127.0.0.1:8080/auth/refresh" \
+		ember server \
 		--environment="development" \
 		--live-reload=false
+
+.PHONY: build-staging
+build-staging: clean ## Build the staging version of the ember app
+	EMBER_GITHUB_APIKEY="caf40f799d653c2ca635" \
+		EMBER_GITHUB_REDIRECT_URI="https://my-staging.tidycat.io" \
+		EMBER_ESA_TOKEN_ENDPOINT="https://api-staging.tidycat.io/auth/token" \
+		EMBER_ESA_REFRESH_ENDPOINT="https://api-staging.tidycat.io/auth/refresh" \
+		ember build \
+		--environment production \
+		--output-path dist/
+	@echo "User-agent: *\nDisallow: /" > dist/robots.txt
+
+.PHONY: build-production
+build-production: clean ## Build the production version of the ember app
+	EMBER_GITHUB_APIKEY="89a218832dc2e39f575b" \
+		EMBER_GITHUB_REDIRECT_URI="https://my.tidycat.io" \
+		EMBER_ESA_TOKEN_ENDPOINT="https://api.tidycat.io/auth/token" \
+		EMBER_ESA_REFRESH_ENDPOINT="https://api.tidycat.io/auth/refresh" \
+		ember build \
+		--environment production \
+		--output-path dist/
 
 .PHONY: eslint
 eslint:  ## Run eslint on the relevant javascript files

commit d242a061f19e39aadf27203830426373068c2898

Fix the environment key name typo
This was accidentally introduced in 813408084052ff95ea3ccb722ea7fcd0aec65abc

config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   // **********
   var ENV = {
     modulePrefix: 'tidycat-ui',
-    environmen: environment,
+    environment: environment,
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
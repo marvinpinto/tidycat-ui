d242a061f19e39aadf27203830426373068c2898
Marvin Pinto <marvin@pinto.im>
2016-04-01 07:59:41 -0400

    Fix the environment key name typo
    This was accidentally introduced in 813408084052ff95ea3ccb722ea7fcd0aec65abc

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   // **********
   var ENV = {
     modulePrefix: 'tidycat-ui',
-    environmen: environment,
+    environment: environment,
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
