f6669544a8026279ab67657c0edbe8535b6de4af
Marvin Pinto <git@pinto.im>
2016-06-12 08:11:00 -0400

    Account for the cases where either the latest_comment_url or the subscription_url could contain null values

--- app/serializers/github-thread.js
+++ app/serializers/github-thread.js
@@ -3,38 +3,43 @@ import DS from 'ember-data';
 export default DS.JSONAPISerializer.extend({
 
   normalizeFindRecordResponse: function(store, type, payload) {
-    return {
+    var response = {
       data: {
-        id: payload.id,
-        type: type.modelName,
-        attributes: {
-          unread: payload.unread,
-          updatedAt: new Date(payload.updated_at),
-          subjectTitle: payload.subject.title,
-          subjectUrl: payload.subject.url,
-          subjectType: payload.subject.type,
-          subjectLatestCommentUrl: payload.subject.latest_comment_url,
-          subscriptionUrl: payload.subscription_url
-        },
-        relationships: {
-          githubSubscription: {
-            data: {
-              id: payload.subscription_url,
-              type: 'github-subscription'
-            }
-          },
-          githubThreadComment: {
-            data: {
-              id: payload.subject.latest_comment_url,
-              type: 'github-thread-comment',
-              attributes: {
-                subjectType: payload.subject.type
-              }
-            }
-          }
-        }
+        attributes: {},
+        relationships: {}
       }
     };
-  }
 
+    response.data.id = payload.id;
+    response.data.type = type.modelName;
+    response.data.attributes = {
+      unread: payload.unread,
+      updatedAt: new Date(payload.updated_at),
+      subjectTitle: payload.subject.title,
+      subjectUrl: payload.subject.url,
+      subjectType: payload.subject.type,
+      subjectLatestCommentUrl: payload.subject.latest_comment_url,
+      subscriptionUrl: payload.subscription_url
+    };
+
+    if (payload.subject.latest_comment_url) {
+      response.data.relationships.githubThreadComment = {
+        data: {
+          id: payload.subject.latest_comment_url,
+          type: 'github-thread-comment'
+        }
+      };
+    }
+
+    if (payload.subscription_url) {
+      response.data.relationships.githubSubscription = {
+        data: {
+          id: payload.subscription_url,
+          type: 'github-subscription'
+        }
+      };
+    }
+
+    return response;
+  }
 });
